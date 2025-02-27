b79a488642f438f038c6adb6327982623dac9aa8
cristofercun97 <cris99991@hotmail.com>
2025-02-27 11:25:41 +0100

    cambio en el congig.py

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv 
 load_dotenv()
 class Config():
-    SECRET_KEY= os.getnv('SECRET_KEY')
+    SECRET_KEY= os.getenv('SECRET_KEY')
     MYSQL_HOST= os.getenv('MYSQL_HOST')
     MYSQL_USER= os.getenv('MYSQL_USER')
     MYSQL_PASSWORD= os.getenv('MYSQL_PASSWORD')
